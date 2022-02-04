88de20ed27c063aaf5e39b61e477cfeb956fa247
Chaitanya Santosh Tekane <chaitanyatekne5@gmail.com>
2022-02-04 14:19:53 +0530

    add functions as props

--- src/component/BlogList.js
+++ src/component/BlogList.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const BlogList = ({ blogs, title }) => {
+const BlogList = ({ blogs, title, handleDelete }) => {
   console.log(blogs);
   console.log(title);
 
@@ -11,6 +11,7 @@ const BlogList = ({ blogs, title }) => {
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written By {blog.author}</p>
+          <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
         </div>
       ))}
     </div>
