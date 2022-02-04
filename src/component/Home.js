277264d8aba9a51aa369c8a354c9f2db6ae987c5
Chaitanya Santosh Tekane <chaitanyatekne5@gmail.com>
2022-02-04 11:00:19 +0530

    add useState Hook

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,27 +1,26 @@
+import { useState } from "react";
+
 const Home = () => {
+  const [name, setName] = useState("Chaitanya");
+  const [age, setAge] = useState(19);
+
   const handleClick = () => {
-    console.log("Button 100 Clicked");
-  };
-
-  const handleClick2 = (name) => {
-    console.log(`${name} Clicked Me`);
-  };
-
-  const handleClick3 = (e) => {
-    console.log(e.target);
-  };
-
-  const handleClick4 = (name, e) => {
-    console.log(`${name} Clicked Me & ${e.target}`);
+    console.log(name + age);
+    setName("Tekane");
+    setAge(20);
+    console.log(name + age);
   };
 
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <button onClick={handleClick}>100</button>
-      <button onClick={() => handleClick2("Rahul")}>200</button>
-      <button onClick={handleClick3}>300</button>
-      <button onClick={(e) => handleClick4("Deepak", e)}>400</button>
+      <h2>
+        My name is {name} and I am {age} years old
+      </h2>
+      <button onClick={handleClick}>Click To Change</button>
+      <p>
+        {name}
+        {age}
+      </p>
     </div>
   );
 };
