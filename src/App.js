31db5c217876d40e9942a711505f7e6bd4772536
Chaitanya Santosh Tekane <chaitanyatekne5@gmail.com>
2022-02-04 09:58:31 +0530

    add multiple components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import "./App.css";
+import Home from "./component/Home";
+import Navbar from "./component/Navbar";
 
 function App() {
-  const title = "Welcome to new blog";
-  const likes = 50;
-  const link = "https://www.google.com/";
-
-  // whatever you are outputting to screen all gets converted into string
-  // we cannot output boolean and object
-  // const persons = { name: "rahul", age: 20 };
-
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-        {/* <p>{persons}</p> */}
-        <p>{10}</p>
-        <p>{"hello chaitanya"}</p>
-        <p>{[1, 2, 3, 4, 5]}</p>
-        <p>{Math.random() * 10}</p>
-        <a href={link} target="_blank">
-          Go To Google
-        </a>
+        <Home />
       </div>
     </div>
   );
