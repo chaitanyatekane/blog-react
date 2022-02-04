66321718a1b183ae3101721d714ae426a79a917b
Chaitanya Santosh Tekane <chaitanyatekne5@gmail.com>
2022-02-04 11:20:59 +0530

    outputting lists on Home component

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,26 +1,25 @@
 import { useState } from "react";
 
 const Home = () => {
-  const [name, setName] = useState("Chaitanya");
-  const [age, setAge] = useState(19);
-
-  const handleClick = () => {
-    console.log(name + age);
-    setName("Tekane");
-    setAge(20);
-    console.log(name + age);
-  };
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
 
   return (
     <div className="home">
-      <h2>
-        My name is {name} and I am {age} years old
-      </h2>
-      <button onClick={handleClick}>Click To Change</button>
-      <p>
-        {name}
-        {age}
-      </p>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written By {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
