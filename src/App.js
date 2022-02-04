4ce93fe1a15f0e6c70350a2ef64b94a215d2f3da
Chaitanya Santosh Tekane <chaitanyatekne5@gmail.com>
2022-02-04 09:50:25 +0530

    new project - dynamic values in template

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import "./App.css";
-import Header from "./Components/Header";
-import Main from "./Components/Main";
-import Footer from "./Components/Footer";
 
 function App() {
+  const title = "Welcome to new blog";
+  const likes = 50;
+  const link = "https://www.google.com/";
+
+  // whatever you are outputting to screen all gets converted into string
+  // we cannot output boolean and object
+  // const persons = { name: "rahul", age: 20 };
+
   return (
     <div className="App">
-      <Header />
-      <Main />
-      <Footer />
+      <div className="content">
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+        {/* <p>{persons}</p> */}
+        <p>{10}</p>
+        <p>{"hello chaitanya"}</p>
+        <p>{[1, 2, 3, 4, 5]}</p>
+        <p>{Math.random() * 10}</p>
+        <a href={link} target="_blank">
+          Go To Google
+        </a>
+      </div>
     </div>
   );
 }
