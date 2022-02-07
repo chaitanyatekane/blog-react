9ad5cb5e80411724dca7fc866aedf243591a207c
Chaitanya Santosh Tekane <chaitanyatekne5@gmail.com>
2022-02-07 09:13:03 +0530

    add dynamic author name

--- src/component/Create.js
+++ src/component/Create.js
@@ -5,7 +5,7 @@ import { useHistory } from "react-router-dom";
 const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
-  const [author, setAuthor] = useState("mario");
+  const [author, setAuthor] = useState("");
   const [isPending, setIsPending] = useState(false);
   const history = useHistory();
 
@@ -29,24 +29,26 @@ const Create = () => {
     <div className="create">
       <h2>Add a New Blog</h2>
       <form onSubmit={handleSubmit}>
-        <label>Blog title:</label>
+        <label>Blog Title :</label>
         <input
           type="text"
           required
           value={title}
           onChange={(e) => setTitle(e.target.value)}
         />
-        <label>Blog body:</label>
+        <label>Blog Body :</label>
         <textarea
           required
           value={body}
           onChange={(e) => setBody(e.target.value)}
         ></textarea>
-        <label>Blog author:</label>
-        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
-          <option value="mario">mario</option>
-          <option value="yoshi">yoshi</option>
-        </select>
+        <label>Blog Author :</label>
+        <input
+          type="text"
+          required
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
         {!isPending && <button>Add Blog</button>}
         {isPending && <button disabled>Adding Blog.....</button>}
       </form>
